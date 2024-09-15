ebb4dab8b3a4ae539a0abae52b76a3211b21bc44
Abril Jarque <abril_jarque@hotmail.com>
2024-09-15 16:43:54 -0300

    commit 6

--- src/App.svelte
+++ src/App.svelte
@@ -121,12 +121,12 @@
 
 @font-face {
   font-family: 'DrukWideMedium';
-  src: url('./tipografia/drukwidemedium.otf') format('opentype');
+  src: url('/public/tipografia/drukwidemedium.otf') format('opentype');
 }
 
 @font-face {
   font-family:'RobotoCondensed-Black';
-  src: url('./tipografia/RobotoCondensed-Black.ttf') format('truetype');
+  src: url('/public/tipografia/RobotoCondensed-Black.ttf') format('truetype');
 }
 
 .ruedas{
